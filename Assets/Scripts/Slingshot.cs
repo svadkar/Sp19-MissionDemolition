a2afaf7394016ade66a849e409b6b36c07a570ef
Sujay Adkar <svadkar725@gmail.com>
2019-02-14 19:42:56 -0500

    Updated Slingshot.cs

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour {
 
+    //fields set in the Unity Inspector Pane
+    public GameObject prefabProjectile;
+    public bool _____________________________;
+    //fields set dynamically
     public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
 
     private void Awake()
     {
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
     }
     void OnMouseEnter()
     {
@@ -22,5 +30,17 @@ public class Slingshot : MonoBehaviour {
     {
         //print("Slingshot: OnMouseExit()");
         launchPoint.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        //The player has pressed the mouse button while over Slingshot
+        aimingMode = true;
+        //Instantiate a Projectile
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        //Start it at launchPoint
+        projectile.transform.position = launchPos;
+        //Set it to isKinematic for now
+        projectile.rigidbody.isKinematic = true;
     }
 }
