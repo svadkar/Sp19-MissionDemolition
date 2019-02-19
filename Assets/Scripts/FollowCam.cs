39f414a003737a0f837503e597ccb725a4072dc4
svadkar <svadkar725@gmail.com>
2019-02-19 10:00:24 -0500

    Worked on FollowCam script more

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -6,6 +6,8 @@ public class FollowCam : MonoBehaviour {
     static public FollowCam S; //a FollowCam Singleton
 
     //field set in the Unity Inspector pane
+    public float easing = 0.05f;
+    public Vector2 minXY;
     public bool _____________________________;
 
     //field set dynamically
@@ -21,20 +23,31 @@ public class FollowCam : MonoBehaviour {
     // Use this for initialization
     void Start ()
     {
+       
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
         if (poi == null) return; //Return if there is no poi
 
         //Get the position of the poi
         Vector3 destination = poi.transform.position;
-        
+
+        //Limit the X & Y to minimum values
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+
+        //Interpolate from the current Camera position toward destination
+        destination = Vector3.Lerp(transform.position, destination, easing);
+
         //Retain a destination.z of camZ
         destination.z = camZ;
 
         //Set the camera to the destination
         transform.position = destination;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+        //Set the orthographicSize of the Camera to keep Ground in view
+        this.camera.orthographicSize = destination.y + 10;
+    }
 }
--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -79,6 +79,7 @@ public class Slingshot : MonoBehaviour {
             aimingMode = false;
             projectile.GetComponent<Rigidbody>().isKinematic = false;
             projectile.GetComponent<Rigidbody>().velocity = -mouseDelta * velocityMult;
+            FollowCam.S.poi = projectile;
             projectile = null;
 
         }
