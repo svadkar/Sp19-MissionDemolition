b6834dd850a81e4653d06525c626fe8ef4e14e56
svadkar <svadkar725@gmail.com>
2019-02-20 22:36:39 -0500

    Updated Goal.cs

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -17,9 +17,9 @@ public class Goal : MonoBehaviour {
             //If so, set goalMet to true
             Goal.goalMet = true;
             //Also set the alpha of the color to higher opacity
-            Color c = renderer.material.color;
+            Color c = GetComponent<Renderer>().material.color;
             c.a = 1;
-            renderer.material.color = c;
+            GetComponent<Renderer>().material.color = c;
         }
     }
 
